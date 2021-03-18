0c5e86f0ceb0c9ca8ba3eeae015460d1629552fc
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-03-18 20:41:54 -0300

    chore: add url and key

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import keys from './keys';
 
 export default function App() {
+  const url =
+    'https://api.openweathermap.org/data/2.5/forecast?lang=pt&units=metric&q=';
+
+  const key = keys.key;
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style='auto' />
     </View>
   );
 }
