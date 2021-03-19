e772dbc61dd17ffb11b22a2393915d6e411c1451
Gabriel Oshiro <gabrieloshiro1997@gmail.com>
2021-03-18 21:28:11 -0300

    feat: call open weather map api

--- App.js
+++ App.js
@@ -1,17 +1,56 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  StyleSheet,
+  TextInput,
+  View,
+  Button,
+  FlatList,
+  Text,
+  Keyboard,
+} from 'react-native';
 import keys from './keys';
 
 export default function App() {
+  const [city, setCity] = useState('');
+  const [forecasts, setForecasts] = useState([]);
+
   const url =
-    'https://api.openweathermap.org/data/2.5/forecast?lang=pt&units=metric&q=';
+    'https://api.openweathermap.org/data/2.5/forecast?lang=pt_br&units=metric&q=';
 
   const key = keys.key;
 
+  const handleChangeCity = (city) => {
+    setCity(city);
+  };
+
+  const getForecasts = () => {
+    setForecasts([]);
+    const target = `${url}${city}&appid=${key}`;
+    console.log(target);
+    fetch(target)
+      .then((response) => response.json())
+      .then((data) => {
+        setForecasts(data['list']);
+        Keyboard.dismiss();
+      });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.input}>
+        <TextInput
+          style={styles.cityName}
+          placeholder='Digite o nome de uma cidade'
+          value={city}
+          onChangeText={handleChangeCity}
+        />
+        <Button title='Ok' onPress={getForecasts}></Button>
+      </View>
+      <FlatList
+        data={forecasts}
+        renderItem={(forecast) => <Text>{JSON.stringify(forecast)}</Text>}
+      ></FlatList>
       <StatusBar style='auto' />
     </View>
   );
@@ -20,8 +59,19 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    padding: 60,
+    flexDirection: 'column',
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    marginBottom: 8,
+  },
+  cityName: {
+    padding: 12,
+    borderBottomColor: '#bb96f3',
+    borderBottomWidth: 2,
+    textAlign: 'center',
+    marginBottom: 6,
+    flexGrow: 0.9,
   },
 });
